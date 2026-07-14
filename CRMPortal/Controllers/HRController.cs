65685ce3699012548e12b30a81fea46421d665a9
aniketbhuse <90091773+aniketbhuse@users.noreply.github.com>
2026-07-14 13:54:14 +0530

    HR controller chnages

--- CRMPortal/Controllers/HRController.cs
+++ CRMPortal/Controllers/HRController.cs
@@ -79,13 +79,13 @@ namespace CRMPortal.Controllers
         {
             try
             {
-<<<<<<< HEAD
+
                 int? roleId = HttpContext.Session.GetInt32("RoleId");
 
                 if (roleId != 5 && roleId != 3)
-=======
+
                 if(HttpContext.Session.GetInt32("RoleId") != 4)
->>>>>>> ebcbf18fa9901ce4bba8197b382adb7da3c7740a
+
                 {
                     return RedirectToAction("Login", "Account");
                 }
